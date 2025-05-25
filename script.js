8478605ade74f2214b5ad223c459735dfd643a62
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-25 22:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const makeChange = (c) => {
   // your name here
+	let result={
+		"q":0,
+		"d":0,
+		"n":0,
+		"p":0
+	}
+	while(c>0)
+	{
+		if(c>=25)
+		{
+			result.q++;
+			c=c-25;
+		}
+		else
+		if(c>=10)
+		{
+			result.d++;
+			c=c-10;
+		}
+		else
+		if(c>=5)
+		{
+			result.n++;
+			c=c-5;
+		}
+		else
+		if(c>=1)
+		{
+			result.p++;
+			c=c-1;
+		}
+	}
+	return result;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
